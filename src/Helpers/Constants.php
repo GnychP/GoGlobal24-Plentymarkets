81945121454b23ffc9a7674392944a0b892034c3
GnychP <patrykgnych@gmail.com>
2020-11-12 17:51:41 +0100

    ServiceNameOther

--- src/Helpers/Constants.php
+++ src/Helpers/Constants.php
@@ -6,7 +6,7 @@ namespace GoGlobal24\Helpers;
 class Constants
 {
     const PLUGIN_NAME = 'GoGlobal24';
-    const SERVICE_NAME = 'DHL';
+    const SERVICE_NAME = 'OTHER_FORWARDER';
     const ENV_PROD = 'prod';
     const ENV_DEV = 'dev';
 
